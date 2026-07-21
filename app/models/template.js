894e8e55eddbe22a09907d76efda8d98b6401e1c
chen21019 <chen21019@gmail.com>
2026-07-21 13:38:46 +0800

    Modernize for PastureStack compatibility

--- app/models/template.js
+++ app/models/template.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import Resource from 'ember-api-store/models/resource';
 import C from 'ui/utils/constants';
+import { localizedCatalogField } from 'ui/utils/localized-catalog-field';
 
 export default Resource.extend({
   projects: Ember.inject.service(),
@@ -89,6 +90,44 @@ export default Resource.extend({
     return this.get('categoryArray').map(x => (x||'').toLowerCase());
   }.property('categoryArray.[]'),
 
+  localizedName: Ember.computed('name', 'labels', 'intl._locale', function() {
+    return localizedCatalogField(
+      this.get('labels'),
+      this.get('intl._locale'),
+      'name',
+      this.get('name')
+    );
+  }),
+
+  localizedDescription: Ember.computed('description', 'labels', 'intl._locale', function() {
+    return localizedCatalogField(
+      this.get('labels'),
+      this.get('intl._locale'),
+      'description',
+      this.get('description')
+    );
+  }),
+
+  isUpstreamFirstParty: Ember.computed('labels', function() {
+    let labels = this.get('labels') || {};
+
+    return labels['io.pasturestack.catalog.origin'] === 'upstream-first-party';
+  }),
+
+  upstreamFirstPartyBadge: Ember.computed(
+    'isUpstreamFirstParty',
+    'intl._locale',
+    function() {
+      if ( this.get('isUpstreamFirstParty') ) {
+        return this.get('intl').t(
+          'catalogPage.index.origin.upstreamFirstParty'
+        );
+      }
+
+      return null;
+    }
+  ),
+
   supported: function() {
     let orch = this.get('projects.current.orchestration')||'cattle';
     if ( this.get('categoryLowerArray').includes('orchestration') ) {
@@ -113,12 +152,12 @@ export default Resource.extend({
     } else {
       return 'thirdparty';
     }
-  }.property('catalogId'),
+  }.property('catalogId', 'labels'),
 
   certifiedClass: function() {
     let type = this.get('certifiedType');
     if ( type === 'rancher' && this.get('settings.isRancher') ) {
-      return 'badge-rancher-logo';
+      return 'badge-pasturestack-logo';
     } else {
       return 'badge-' + type;
     }
@@ -138,7 +177,7 @@ export default Resource.extend({
     }
 
     if ( this.get('catalogId') !== C.CATALOG.LIBRARY_KEY && (out === C.LABEL.CERTIFIED_RANCHER || looksLikeCertified) ) {
-      // Rancher-certified things can only be in the library catalog.
+      // Platform-certified things can only be in the library catalog.
       out = null;
     }
 
